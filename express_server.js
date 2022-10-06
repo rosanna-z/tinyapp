dda73169f7eae772b869fcc227b80dba9767a525
Rosanna Zhang <rosannazhang92@gmail.com>
2022-10-05 21:04:53 -0700

    Add basic permissions features

--- express_server.js
+++ express_server.js
@@ -28,12 +28,13 @@ const users = {
 
 function findUserbyEmail(email) {
   for (const userId in users) {
-  const userDb = users[userId];
-  if (userDb.email === email) {
-  return userDb;
+    const userDb = users[userId];
+    if (userDb.email === email) {
+      return userDb;
+    }
+    return null;
   }
-  return null;
-}}
+}
 
 function generateRandomString() {
   let string = '';
@@ -57,43 +58,59 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const email = users[req.cookies["user_id"]]?.email
-  const templateVars = { urls: urlDatabase, email } ;
+  const email = users[req.cookies["user_id"]]?.email;
+  const templateVars = { urls: urlDatabase, email };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const email = users[req.cookies["user_id"]]?.email
-  const templateVars = { email } ;
+  const email = users[req.cookies["user_id"]]?.email;
+  const templateVars = { email };
+  if (!email) {
+    return res.redirect(`/login`);
+  }
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+    if (!urlDatabase[req.params.id]) {
+      return res.send("This shorten URL does not exist.")
+    }
+  
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], email: users[req.cookies["user_id"]]?.email };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
+  const email = users[req.cookies["user_id"]]?.email;
+  if (!email) {
+    return res.send("<div>You are not logged in.</div>")
+  }
   const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL
-  res.redirect(`/urls/${id}`); 
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
 });
 
 app.get("/register", (req, res) => {
   const email = users[req.cookies["user_id"]]?.email;
-  const templateVars = { email } ;
+  const templateVars = { email };
+
+  if (email) {
+    res.redirect(`/urls`);
+  }
+
   res.render("registration", templateVars);
 });
 
 app.post("/register", (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
+  const email = req.body.email;
+  const password = req.body.password;
 
   if (!email || !password) {
     return res.status(400).send('Please include both email and password');
   }
 
-  const userDb = findUserbyEmail(email)
+  const userDb = findUserbyEmail(email);
   if (userDb) {
     return res.status(400).send('Sorry! This email is taken!');
   }
@@ -106,38 +123,43 @@ app.post("/register", (req, res) => {
     password
   };
 
-  res.cookie('user_id', newUserId)
-  res.redirect(`/urls`); 
+  res.cookie('user_id', newUserId);
+  res.redirect(`/urls`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   delete urlDatabase[id];
-  res.redirect(`/urls`); 
+  res.redirect(`/urls`);
 });
 
 app.post("/urls/:id/edit", (req, res) => {
-  const newURL = req.body.newURL
-  const id = req.params.id
-  urlDatabase[id] = newURL
-  res.redirect(`/urls`); 
+  const newURL = req.body.newURL;
+  const id = req.params.id;
+  urlDatabase[id] = newURL;
+  res.redirect(`/urls`);
 });
 
 app.get("/login", (req, res) => {
   const email = users[req.cookies["user_id"]]?.email;
-  const templateVars = { email } ;
+  const templateVars = { email };
+
+  if (email) {
+    res.redirect(`/urls`);
+  }
+
   res.render("login", templateVars);
 });
 
 app.post("/login", (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
+  const email = req.body.email;
+  const password = req.body.password;
 
   if (!email || !password) {
     return res.status(400).send('Please include both email and password.');
   }
 
-  const user = findUserbyEmail(email)
+  const user = findUserbyEmail(email);
   if (!user) {
     return res.status(403).send('This email does not exist.');
   }
@@ -148,22 +170,22 @@ app.post("/login", (req, res) => {
 
   res.cookie('user_id', user.id);
 
-  res.redirect(`/urls`); 
+  res.redirect(`/urls`);
 });
 
 app.post("/urls/login", (req, res) => {
-  const email = req.body.email
-  res.cookie('user_id', userDb.id)
-  res.redirect(`/urls`); 
+  const email = req.body.email;
+  res.cookie('user_id', userDb.id);
+  res.redirect(`/urls`);
 });
 
 app.post("/urls/logout", (req, res) => {
-  res.clearCookie('user_id', req.body.email)
-  res.redirect(`/urls`); 
+  res.clearCookie('user_id', req.body.email);
+  res.redirect(`/urls`);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
+  const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
